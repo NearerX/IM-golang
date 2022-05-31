037520b1a330df829e73e8c3c6eaa284eb21d38a
NX <564394806@qq.com>
2022-06-01 00:15:39 +0800

    v8

--- server.go
+++ server.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net"
 	"sync"
+	"time"
 )
 
 type Server struct {
@@ -30,15 +32,39 @@ func (this *Server) BroadCast(user *User, msg string) {
 }
 
 func (this *Server) Handler(conn net.Conn) {
-	user := NewUser(conn)
-	this.mapLock.Lock()
-	this.OnlineMap[user.Name] = user
-	this.mapLock.Unlock()
+	user := NewUser(conn, this)
+	user.Online()
 
-	this.BroadCast(user, "online")
+	isLive := make(chan bool)
+	//接受客户端发送的消息
+	go func() {
+		buf := make([]byte, 4096)
+		for {
+			len, err := conn.Read(buf)
+			if err != nil && err != io.EOF {
+				fmt.Println("Conn read error:", err)
+				return
+			}
+			if len == 0 {
+				user.Offline()
+				return
+			}
+			msg := string(buf[:len-1])
+			user.DoMessage(msg)
+			isLive <- true
+		}
+	}()
 
-	//当前handler阻塞
-	select {}
+	for {
+		select {
+		case <-isLive:
+		case <-time.After(time.Second * 600):
+			user.SendMsg("you ara out\n")
+			close(user.Chan)
+			conn.Close()
+			return
+		}
+	}
 }
 
 func (this *Server) ListenMessage() {
--- user.go
+++ user.go
@@ -1,12 +1,16 @@
 package main
 
-import "net"
+import (
+	"net"
+	"strings"
+)
 
 type User struct {
-	Name string
-	Addr string
-	Chan chan string
-	conn net.Conn
+	Name   string
+	Addr   string
+	Chan   chan string
+	conn   net.Conn
+	server *Server
 }
 
 func (this *User) ListenMessage() {
@@ -16,15 +20,93 @@ func (this *User) ListenMessage() {
 	}
 }
 
-func NewUser(conn net.Conn) *User {
+func NewUser(conn net.Conn, server *Server) *User {
 	userAddr := conn.RemoteAddr().String()
 
 	user := &User{
-		Name: userAddr,
-		Addr: userAddr,
-		Chan: make(chan string),
-		conn: conn,
+		Name:   userAddr,
+		Addr:   userAddr,
+		Chan:   make(chan string),
+		conn:   conn,
+		server: server,
 	}
 	go user.ListenMessage()
 	return user
+}
+
+func (this *User) Online() {
+	this.server.mapLock.Lock()
+	this.server.OnlineMap[this.Name] = this
+	this.server.mapLock.Unlock()
+
+	this.server.BroadCast(this, "login online...")
+}
+
+func (this *User) Offline() {
+	this.server.mapLock.Lock()
+	delete(this.server.OnlineMap, this.Name)
+	this.server.mapLock.Unlock()
+
+	this.server.BroadCast(this, "offline...")
+}
+
+func (this *User) DoMessage(msg string) {
+	if msg == "who" {
+		this.server.mapLock.Lock()
+		for _, user := range this.server.OnlineMap {
+			onlineMsg := "[" + user.Addr + "]" + user.Name + ":" + "在线...\n"
+			this.SendMsg(onlineMsg)
+		}
+		this.server.mapLock.Unlock()
+	} else if len(msg) > 7 && msg[:7] == "rename|" {
+		//消息格式: rename|张三
+		newName := strings.Split(msg, "|")[1]
+
+		//判断name是否存在
+		_, ok := this.server.OnlineMap[newName]
+		if ok {
+			this.SendMsg("当前用户名被使用\n")
+		} else {
+			this.server.mapLock.Lock()
+			delete(this.server.OnlineMap, this.Name)
+			this.server.OnlineMap[newName] = this
+			this.server.mapLock.Unlock()
+
+			this.Name = newName
+			this.SendMsg("您已经更新用户名:" + this.Name + "\n")
+		}
+
+	} else if len(msg) > 4 && msg[:3] == "to|" {
+		//消息格式:  to|张三|消息内容
+
+		//1 获取对方的用户名
+		remoteName := strings.Split(msg, "|")[1]
+		if remoteName == "" {
+			this.SendMsg("消息格式不正确，请使用 \"to|张三|你好啊\"格式。\n")
+			return
+		}
+
+		//2 根据用户名 得到对方User对象
+		remoteUser, ok := this.server.OnlineMap[remoteName]
+		if !ok {
+			this.SendMsg("该用户名不不存在\n")
+			return
+		}
+
+		//3 获取消息内容，通过对方的User对象将消息内容发送过去
+		content := strings.Split(msg, "|")[2]
+		if content == "" {
+			this.SendMsg("无消息内容，请重发\n")
+			return
+		}
+		remoteUser.SendMsg(this.Name + "对您说:" + content)
+
+	} else {
+		this.server.BroadCast(this, msg)
+	}
+
+}
+
+func (this *User) SendMsg(msg string) {
+	this.conn.Write([]byte(msg))
 }
